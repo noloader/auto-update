b3c4412547e5ab8c72ce7ba3f7d7058eabd13d3d
Jeffrey Walton <noloader@gmail.com>
2019-06-18 16:58:35 -0400

    Add Ubuntu support

--- install.sh
+++ install.sh
@@ -5,22 +5,39 @@ if [[ "$EUID" -ne 0 ]]; then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-if lsb_release -a | grep -q 'Fedora'
-then
-    echo "Installing on Fedora"
-    cp auto-update.service /etc/systemd/system
-    cp auto-update.timer /etc/systemd/system
-    cp -T auto-update.dnf /usr/sbin/auto-update
-elif lsb_release -a | grep -q 'Debian'
-then
-    echo "Installing on Debian"
-    cp auto-update.service /etc/systemd/system
-    cp auto-update.timer /etc/systemd/system
-    cp -T auto-update.apt /usr/sbin/auto-update
-else
-    echo "Unkown operating system"
-    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
-fi
+lsb_name=$(lsb_release -a | awk -F ':' '$1 == "Distributor ID" {print $2}')
+os_name=$(echo $lsb_name | tr '[:upper:]' '[:lower:]')
+echo "Operating system: $os_name"
+
+case "$os_name" in
+    "fedora")
+        echo "Installing on Fedora"
+        cp auto-update.service /etc/systemd/system
+        cp auto-update.timer /etc/systemd/system
+        cp -T auto-update.dnf /usr/sbin/auto-update
+        ;;
+    "red hat")
+        echo "Installing on Fedora"
+        cp auto-update.service /etc/systemd/system
+        cp auto-update.timer /etc/systemd/system
+        cp -T auto-update.dnf /usr/sbin/auto-update
+        ;;
+    "debian")
+        echo "Installing on Fedora"
+        cp auto-update.service /etc/systemd/system
+        cp auto-update.timer /etc/systemd/system
+        cp -T auto-update.apt /usr/sbin/auto-update
+        ;;
+    "ubuntu")
+        echo "Installing on Fedora"
+        cp auto-update.service /etc/systemd/system
+        cp auto-update.timer /etc/systemd/system
+        cp -T auto-update.apt /usr/sbin/auto-update
+        ;;
+    *)
+        echo "Unkown operating system"
+        [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+esac
 
 if ! systemctl enable auto-update.service; then
     echo "Failed to enable auto-update.service"
@@ -37,4 +54,5 @@ if ! systemctl start auto-update.timer; then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
+echo "Installed services"
 [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
